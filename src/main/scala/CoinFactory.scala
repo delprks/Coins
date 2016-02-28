2875638485f7ad0b86d3aef97e6c20a445d198c9
Daniel Parks <daniel.parks@bbc.co.uk>
2016-02-28 21:11:45 +0000

    added 2 pound coin!

--- src/main/scala/CoinFactory.scala
+++ src/main/scala/CoinFactory.scala
@@ -5,7 +5,7 @@ object CoinFactory {
   def getCoins(pences: Int) = {
     var mPences = pences
     val coinMap = mutable.Map.empty[String, Int]
-    val coins: Map[String, Int] = ListMap("OnePound" -> 100, "FiftyPence" -> 50, "TwentyPence" -> 20, "TenPence" -> 10, "FivePence" -> 5, "TwoPence" -> 2, "OnePence" -> 1)
+    val coins: Map[String, Int] = ListMap("TwoPound" -> 200, "OnePound" -> 100, "FiftyPence" -> 50, "TwentyPence" -> 20, "TenPence" -> 10, "FivePence" -> 5, "TwoPence" -> 2, "OnePence" -> 1)
 
     coins.foreach(coin => {
       var numOfCoins = 0
