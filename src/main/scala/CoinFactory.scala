7202440782aaac938bcd227ea3de20a54ca5b297
Daniel Parks <daniel.parks@bbc.co.uk>
2016-02-28 21:04:46 +0000

    ugly working version

--- src/main/scala/CoinFactory.scala
+++ src/main/scala/CoinFactory.scala
@@ -1,26 +1,21 @@
+import scala.collection.immutable.ListMap
 import scala.collection.mutable
-
-trait Coin
-
-case class OnePence(val value: Int = 1) extends Coin
-case class TwoPence(val value: Int = 2) extends Coin
-case class FivePence(val value: Int = 5) extends Coin
-case class TenPence(val value: Int = 10) extends Coin
-case class TwentyPence(val value: Int = 20) extends Coin
-case class FiftyPence(val value: Int = 50) extends Coin
-case class OnePound(val value: Int = 100) extends Coin
 
 object CoinFactory {
   def getCoins(pences: Int) = {
+    var mPences = pences
     val coinMap = mutable.Map.empty[String, Int]
-    val coins = List(OnePound, FiftyPence, TwentyPence, TenPence, FiftyPence, TwoPence, OnePence)
+    val coins: Map[String, Int] = ListMap("OnePound" -> 100, "FiftyPence" -> 50, "TwentyPence" -> 20, "TenPence" -> 10, "FivePence" -> 5, "TwoPence" -> 2, "OnePence" -> 1)
 
-    for (coin <- coins) {
-      if (coinMap.contains("test"))
-        coinMap += ("test" -> (coinMap("test") + 1))
-      else coinMap("test") = 0
-    }
+    coins.foreach(coin => {
+      var numOfCoins = 0
 
-    coinMap
+      while(mPences >= coin._2) {
+        mPences = mPences - coin._2
+        coinMap(coin._1) = numOfCoins + 1
+      }
+    })
+
+    coinMap.toList
   }
 }
